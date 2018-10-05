e9d018f88f665a0f51f4e28f640be91d43d84466
Johan <johan@groenewald.co.za>
2018-10-05 02:02:07 +0200

    Developing a tracking rectangle

--- Vitonomous/experiments/stream.py
+++ Vitonomous/experiments/stream.py
@@ -12,7 +12,7 @@ wait_delay = 2
 
 capture = cv2.VideoCapture(url)
 
-hist_image = np.zeros((256, 256), dtype=np.uint8)
+re_boundary = None
 grabbed, re_frame = capture.read()
 while grabbed:
     grabbed, frame_in = capture.read()
@@ -20,23 +20,35 @@ while grabbed:
         print('Error grabbing frame')
         break
 
+    frame_out = frame_in.copy()
     gray_frame_in = cv2.cvtColor(frame_in, cv2.COLOR_BGR2GRAY)
     gray_re_frame = cv2.cvtColor(re_frame, cv2.COLOR_BGR2GRAY)
 
     diff_frame = cv2.absdiff(gray_frame_in, gray_re_frame)
 
-    # hist = cv2.calcHist([diff_frame], [0], None, [256], [0, 256])
-    hist, bins = np.histogram(diff_frame.ravel(), 256, [0, 256])
+    raw_indices = np.nonzero(diff_frame >= 32)
+    indices = [(x,y) for x,y in zip(raw_indices[1], raw_indices[0])]
+    indices = [] if indices is None else indices
+    if 550 < len(indices) < 1050:
+        boundary = cv2.boundingRect(np.array(indices)) if indices else None
+    else:
+        boundary = re_boundary
+    if boundary and re_boundary:
+        bw, bh = 128, 128
+        bx, by = boundary[0:2]
+        rbx, rby = re_boundary[0:2]
+        pivot = 25
+        bx = rbx if abs(bx-rbx) > pivot else bx
+        by = rby if abs(by-rby) > pivot else by
+        cv2.rectangle(
+            frame_out, (bx, by), (bx+bw, by+bh),
+            (255, 255, 255), 1
+        )
+        boundary = (bx, by, 0, 0)
+    re_boundary = boundary
 
-    hist_image.fill(255)
-    for i, h in enumerate(hist):
-        # hist_image[i:i+1, 0:h] = 255
-        hist_image[0:h, i:i+1] = 0
-    hist_image = hist_image.transpose((0, 1))
-
-    frame_out = diff_frame
+    # frame_out = diff_frame
     cv2.imshow("IP Camera", frame_out)
-    cv2.imshow("Histogram", hist_image)
     if cv2.waitKey(delay=wait_delay) & 0xFF == ord('q'):
         break
 
